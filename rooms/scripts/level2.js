b821a57b7330026c1affada266472893b2696b31
iZnoR <76132625+iznor@users.noreply.github.com>
2022-01-04 16:36:35 +0200

    escape-room

---
+++ rooms/scripts/level2.js
@@ -0,0 +1,3 @@
+$('#to-second').click(function () {
+    window.open("https://www.w3schools.com/jsref/jsref_link.asp")
+});
--- rooms/scripts/modals.js
+++ rooms/scripts/modals.js
@@ -11,9 +11,9 @@ $('#instructions').click(function () {
   $('#myModal').css('display', 'block');
   $('#mod-h1').append('ברוכים הבאים לחדר הבריחה');
   $('#mod-h2').append('הוראות');
-  $('#mod-p').append('חדר הבריחה הוירטואלי שלפניכם כולל תמונה ושלושה רמזים.<br>המשחק כולל 2 שלבים.<br>עם סיום השלב הראשון תועברו אוטומטית לשלב השני והאחרון.<br>בתמונה מסתתרים שלושה אובייקטים שעליכם למצוא.<br>הכרטיסיות מימין ירמזו לכם על חפץ או אובייקט כלשהו בתמונה.<br>בהתאם לרמזים שתקבלו, נסו לזהות את האובייקט הרלוונטי בתמונה ולחצו עליו ב"קליק".<br>אם צדקתם ו"הקלקתם" במקום הנכון, תופיע על המסך דמות, לחצו עליה והיא תספר לכם עובדה מיוחדת אודות היחידה לחדשנות בחברת החשמל.');
+  $('#mod-p').append('חדר הבריחה הוירטואלי שלפניכם כולל תמונה ושלושה רמזים.<br>המשחק כולל 2 שלבים.<br>עם סיום השלב הראשון תועברו אוטומטית לשלב השני והאחרון.<br>בתמונה מסתתרים שלושה אובייקטים שעליכם למצוא.<br>הכרטיסיות מימין ירמזו לכם על חפץ או אובייקט כלשהו בתמונה.<br>בהתאם לרמזים שתקבלו, נסו לזהות את האובייקט הרלוונטי בתמונה ולחצו עליו ב"קליק".<br>אם צדקתם ו"הקלקתם" במקום הנכון, תופיע על המסך דמות, לחצו עליה והיא תספר לכם עובדה מיוחדת אודות הספארי של רמת גן.');
   $('.modal-content p').css('justify-content', 'right')
-  .css('width','82%')
+    .css('width', '82%')
 
 })
 $('#obj-three').click(function () {
@@ -21,16 +21,16 @@ $('#obj-three').click(function () {
   if (obj_three_click > 1) {
     $('#mod-h1').text('');
     $('#mod-h2').text('');
-    if(level==1){
-    if ($('#modal-link').text('<a id="modal-link" href="http://www.metrycom.com/"target="_blank">http://www.metrycom.com/</a>')){
-      $('#modal-link').remove();
+    if (level == 1) {
+      if ($('#modal-link').text('<a id="modal-link" href="http://www.metrycom.com/"target="_blank">http://www.metrycom.com/</a>')) {
+        $('#modal-link').remove();
       }
-    $('#mod-h2').css('text-align', 'right');
-    $('#mod-h2').css('width', '82%');
-    $('#mod-p').text('');
-    $('#myModal').css('display', 'block');
-    $('.modal-content h1').append('הידעת?!');
-    $('.modal-content p').append('חברת החשמל מספקת אנרגיה מצפון המדינה ועד לדרום.\
+      $('#mod-h2').css('text-align', 'right');
+      $('#mod-h2').css('width', '82%');
+      $('#mod-p').text('');
+      $('#myModal').css('display', 'block');
+      $('.modal-content h1').append('הידעת?!');
+      $('.modal-content p').append('חברת החשמל מספקת אנרגיה מצפון המדינה ועד לדרום.\
                                   על מנת לעמוד ביעד זה מערך הרשת, נמצא פרוס ברחבי המדינה בהתאם (המערך כל כך ארוך ועצום עד כדי כך שאם נפרוס את רשת ההולכה והחלוקה\
                                     –נצליח להקיף פעמיים את כדור הארץ!). כפועל יוצא מכך, חברת החשמל סבלה לא פעם מגניבות כבלים וציוד יקר ערך,\
                                     אך השיא הגיע כאשר גניבת הכבלים הגדולה במדינה קרתה דווקא אצלנו, \ועשרות קילומטרים של כבלים בדרום המדינה נגנבו על ידי שודדים.\
@@ -38,27 +38,27 @@ $('#obj-three').click(function () {
                                     במטרה לאתר בזמן מפגעים ובעיות מסוג זה.\
                                   לקריאה מורחבת היכנסו לאתר החברה:<br>\
                                 ');
-    if ($('#modal-link').text('')){
-    $('.modal-content').append('<a id="modal-link" href="http://www.metrycom.com/"target="_blank">http://www.metrycom.com/</a>');
+      if ($('#modal-link').text('')) {
+        $('.modal-content').append('<a id="modal-link" href="http://www.metrycom.com/"target="_blank">http://www.metrycom.com/</a>');
+      }
+      $('#modal-link').css('color', 'blue');
+      $('.modal-content h2').css('justify-content', 'right');
+      $('.modal-content p').css('justify-content', 'right')
+        .css('width', '82%');
     }
-    $('#modal-link').css('color','blue');
-    $('.modal-content h2').css('justify-content', 'right');
-    $('.modal-content p').css('justify-content', 'right')
-    .css('width','82%');
-  }
-  else if (level==2) {
-    $('#mod-h1').text('');
-    $('#mod-h2').text('');
-    $('#mod-p').text('');
-    $('#modal-link').text('');
-    $('#myModal').css('display', 'block');
-    $('.modal-content h1').append('הידעת?!');
-    $('.modal-content p').append('גניבת הכבלים הגדולה במדינה, קרתה דווקא בחברת החשמל, כשגנבים גנבו עשרות קילומטרים של כבלים בדרום הארץ. כדי להתמודד עם אתגרים אלו, נעשה פיילוט עם חברת מטריקום וע"י שימוש בטכנולוגיה של IOT .ניתן לקבל התראות על מצב הכבלים ולמנוע גניבות');
-    $('.modal-content p').css('justify-content', 'right')
-    .css('width','82%')
-    
-  }
-  }else{
+    else if (level == 2) {
+      $('#mod-h1').text('');
+      $('#mod-h2').text('');
+      $('#mod-p').text('');
+      $('#modal-link').text('');
+      $('#myModal').css('display', 'block');
+      $('.modal-content h1').append('הידעת?!');
+      $('.modal-content p').append('גניבת הכבלים הגדולה במדינה, קרתה דווקא בחברת החשמל, כשגנבים גנבו עשרות קילומטרים של כבלים בדרום הארץ. כדי להתמודד עם אתגרים אלו, נעשה פיילוט עם חברת מטריקום וע"י שימוש בטכנולוגיה של IOT .ניתן לקבל התראות על מצב הכבלים ולמנוע גניבות');
+      $('.modal-content p').css('justify-content', 'right')
+        .css('width', '82%')
+
+    }
+  } else {
     $('#mod-h1').text('');
     $('#mod-h2').text('');
     $('#mod-p').text('');
@@ -67,12 +67,12 @@ $('#obj-three').click(function () {
     $('#myModal').css('display', 'block');
     $('.modal-content h1').append('יפה!');
     $('.modal-content p').css('justify-content', 'center')
-                          .css('width','62%')
+      .css('width', '62%')
     $('.modal-content p').append('');
-    if (level==1) {
-      $('.modal-content p').append('לחצו על הטכנאי כדי לגלות עובדה מעניינת');
+    if (level == 1) {
+      $('.modal-content p').append('אני התנין !');
     }
-    else if (level==2) {
+    else if (level == 2) {
       $('.modal-content p').append('לחצו על הגנב כדי לגלות עובדה מעניינת');
     }
   }
@@ -80,35 +80,35 @@ $('#obj-three').click(function () {
 $('#obj-two').click(function () {
   ++obj_two_click;
   if (obj_two_click > 1) {
-    if (level==1) {
-    $('#mod-h1').text('');
-    $('#mod-h2').text('');
-    $('#mod-p').text('');
-    $('#modal-link').text('');
-    $('#myModal').css('display', 'block');
-    $('.modal-content h1').append('הידעת?!');
-    $('.modal-content p').append('בחברת החשמל בשנה האחרונה נעשה יותר מ-10 פיילוטים עם 4 חטיבות – לוגסטיקה, יצור, רשת ופרויקטים בעזרת טכנולוגית רחפנים. בנושאי ניטור, שינוע, אבטחה ועוד.\
+    if (level == 1) {
+      $('#mod-h1').text('');
+      $('#mod-h2').text('');
+      $('#mod-p').text('');
+      $('#modal-link').text('');
+      $('#myModal').css('display', 'block');
+      $('.modal-content h1').append('הידעת?!');
+      $('.modal-content p').append('בחברת החשמל בשנה האחרונה נעשה יותר מ-10 פיילוטים עם 4 חטיבות – לוגסטיקה, יצור, רשת ופרויקטים בעזרת טכנולוגית רחפנים. בנושאי ניטור, שינוע, אבטחה ועוד.\
                                   רחפנים הינה טכנולוגיה אשר יכולה להיות בשימוש לצרכים ולפתרון אתגרים מגוונים,\
                                   זו אולי אחת הסיבות כי הוקם בחברת החשמל וועדה מיוחדת אשר מטרתה לשלב ולבחון טכנולוגיה זו,\
                                     לפתרון בעיות ולהבאת ערך לחברה. כך למשל, אנו יכולים לראות כי בשנה אשתקד,\
                                   נעשו בחברה יותר מ-10 פיילוטים שונים בעזרת הטכנולוגיה ועם יותר מ-4 חטיבות שונות – לוגסטיקה ותפעול, ייצור רשת וגם חטיבת הפרויקטים.\
                                   הנושאים השונים בהם היה התנסות של הטכנולוגיה היו מגוונים בהתאם, וכללו בין השאר ניטור, שינוע, אבטחה ועוד.\
                                   ');
-    $('.modal-content p').css('justify-content', 'right')
-    .css('width','82%')
-  }else if (level==2) {
-    $('#mod-h1').text('');
-    $('#mod-h2').text('');
-    $('#mod-p').text('');
-    $('#modal-link').text('');
-    $('#myModal').css('display', 'block');
-    $('.modal-content h1').append('הידעת?!');
-    $('.modal-content p').append('החדשנות הראשונה בארץ בתחום החשמל נעשתה על ידי רוטנברג, שהיה יזם פורץ דרך עם רעיון חולמני להקים מפעל ליצור חשמל.');
-    $('.modal-content p').css('justify-content', 'right')
-    .css('width','82%')
+      $('.modal-content p').css('justify-content', 'right')
+        .css('width', '82%')
+    } else if (level == 2) {
+      $('#mod-h1').text('');
+      $('#mod-h2').text('');
+      $('#mod-p').text('');
+      $('#modal-link').text('');
+      $('#myModal').css('display', 'block');
+      $('.modal-content h1').append('הידעת?!');
+      $('.modal-content p').append('החדשנות הראשונה בארץ בתחום החשמל נעשתה על ידי רוטנברג, שהיה יזם פורץ דרך עם רעיון חולמני להקים מפעל ליצור חשמל.');
+      $('.modal-content p').css('justify-content', 'right')
+        .css('width', '82%')
     }
 
-  }else{
+  } else {
     $('#mod-h1').text('');
     $('#mod-h2').text('');
     $('#mod-p').text('');
@@ -116,12 +116,12 @@ $('#obj-two').click(function () {
     $('#myModal').css('display', 'block');
     $('.modal-content h1').append('יפה!');
     $('.modal-content p').css('justify-content', 'center')
-    .css('width','62%');
+      .css('width', '62%');
     $('.modal-content p').append('');
-    if (level==1) {
-      $('.modal-content p').append('לחצו על הרחפן בתמונה כדי לגלות עובדה מעניינת !');
+    if (level == 1) {
+      $('.modal-content p').append('אנחנו הגורילות זקוקים לעזרה ברבייה ולכן אנו נעזרים בארגון האירופאי');
     }
-    else if (level==2) {
+    else if (level == 2) {
       $('.modal-content p').append('לחצו על כדור הארץ כדי לגלות עובדה מעניינת !');
     }
   }
@@ -129,17 +129,17 @@ $('#obj-two').click(function () {
 $('#obj-one').click(function () {
   ++obj_one_click;
   if (obj_one_click > 1) {
-    if (level==1) {
-    $('#mod-h1').text('');
-    $('#mod-h2').text('');
-    $('#mod-p').text('');
-    $('#modal-link').remove();
-    $('#myModal').css('display', 'block');
-    $('.modal-content h1').append('הידעת?!');
-    $('.modal-content p').append('יחידת החדשנות בחברת החשמל מונה  5 עובדים בלבד !');
-    $('.modal-content p').css('justify-content', 'right')
-    .css('width','82%');
-    }else if (level==2) {
+    if (level == 1) {
+      $('#mod-h1').text('');
+      $('#mod-h2').text('');
+      $('#mod-p').text('');
+      $('#modal-link').remove();
+      $('#myModal').css('display', 'block');
+      $('.modal-content h1').append('הידעת?!');
+      $('.modal-content p').append('יחידת החדשנות בחברת החשמל מונה  5 עובדים בלבד !');
+      $('.modal-content p').css('justify-content', 'right')
+        .css('width', '82%');
+    } else if (level == 2) {
       $('#mod-h1').text('');
       $('#mod-h2').text('');
       $('#mod-p').text('');
@@ -148,10 +148,10 @@ $('#obj-one').click(function () {
       $('.modal-content h1').append('הידעת?!');
       $('.modal-content p').append("בראש היחידה לחדשנות עומדת הגב' דורין ברבי, אשר משמשת כדוגמה מדהימה להצלחה בתחום היזמות. פועלה של ברבי הידהד בכל העולם ואף פורסמה עליה כתבה מפוארת במגזין העולמי Forbes תחת הקטגוריה של 30 מתחת ל-30, המציגה את סיפורם.ן של יזמים.ות מכל העולם, אשר פועלם.ן פרץ לתודעה באופן בולט במיוחד. אין ספק שהיחידה לחדשנות בחברת החשמל נמצאת בידיים טובות.");
       $('.modal-content p').css('justify-content', 'right')
-      .css('width','82%')
-      }
+        .css('width', '82%')
+    }
 
-  }else{
+  } else {
     $('#mod-h1').text('');
     $('#mod-h2').text('');
     $('#mod-p').text('');
@@ -160,11 +160,11 @@ $('#obj-one').click(function () {
     $('.modal-content h1').append('יפה !');
     $('.modal-content p').append('');
     $('.modal-content p').css('justify-content', 'center')
-    .css('width','62%');
-    if (level==1) {
-      $('.modal-content p').append('לחצו על קבוצת האנשים הכחולים בתמונה כדי לגלות עובדה מעניינת !');
+      .css('width', '62%');
+    if (level == 1) {
+      $('.modal-content p').append('אנחנו העופות מככבים בביקורים אצל הוטרינר, ומגיעים לעיתים תכופות לביה"ח בספארי');
     }
-    else if (level==2) {
+    else if (level == 2) {
       $('.modal-content p').append('לחצו על &nbsp;&nbsp;<i>"Forbes 30 under 30"</i>&nbsp;&nbsp; כדי לגלות עובדה מעניינית !');
     }
 
